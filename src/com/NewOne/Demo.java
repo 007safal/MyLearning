95981e8e9eb8120a41afe8de48196e11a5b17fd0
safal <msafal007@gmail.com>
2023-02-25 20:21:10 -0600

    wasted file

--- src/com/NewOne/Demo.java
+++ src/com/NewOne/Demo.java
@@ -39,7 +39,7 @@ public class Demo {
         int number = scanner.nextInt();
         for (int i = 1; i<=10 ; i++) {
             System.out.println(number + "X" + i + "=" + (number * i));
-
+            System.out.println("");
         }
 
 
